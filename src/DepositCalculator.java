1ca0e868d521e58c0071e1b1dc0ea1932c49fac4
Arkadiy Skorobogatov <skoroara@yandex.ru>
2023-07-01 17:39:25 +0300

    fix: add undefined action case

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 public class DepositCalculator {
     double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
         double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
-
         return calculateRound(pay);
     }
 
     double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
         double pay = amount + amount * yearRate * depositPeriod;
-
         return calculateRound(pay);
     }
 
     double calculateRound(double value) {
         double scale = Math.pow(10, 2);
-
         return Math.round(value * scale) / scale;
     }
 
@@ -31,8 +28,8 @@ public class DepositCalculator {
         amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         depositPeriod = scanner.nextInt();
-        System.out.println("Введите годовую ставку:");
-        yearRate = scanner.nextInt();
+        System.out.println("Введите годовую ставку в процентах:");
+        yearRate = scanner.nextInt() / 100.0;
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
 
@@ -41,7 +38,8 @@ public class DepositCalculator {
         } else if (action == 2) {
             pay = calculateComplexPercent(amount, yearRate, depositPeriod);
         } else {
-            pay = 0;
+            System.out.println("Вклада с таким идентификатором не существует");
+            return;
         }
 
         System.out.println("Результат вклада: " + amount + " за " + depositPeriod + " лет превратятся в " + pay);
